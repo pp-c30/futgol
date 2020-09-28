9f003ba476546eecbd2f92e9aed100ab4180b2b8
luis44120 <luismora44120@gmail.com>
2020-09-28 12:58:25 -0300

    subida de imagen

--- build/routes/galeria.routes.js
+++ build/routes/galeria.routes.js
@@ -1,1 +1,6 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const enrutadorGaleria = express_1.Router();
+enrutadorGaleria.route('/galeria').post();
+exports.default = enrutadorGaleria;
