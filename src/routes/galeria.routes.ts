c131ef5fc14f3db5a14154c61f18ffda97ef285e
luis44120 <luismora44120@gmail.com>
2020-09-28 15:17:41 -0300

    se elimina ruta

--- src/routes/galeria.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-
-const enrutadorGaleria = Router();
-
-enrutadorGaleria.route('/galeria').post();
-
-export default enrutadorGaleria;
--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,6 @@ import morgan  from "morgan";
 
 import cors  from "cors";
 
-import  enrutadorGaleria  from "./routes/galeria.routes";
-
 import enrutadorIndex from "./routes/index.route";
 
 import enrutadorAsistencia  from "./routes/asistencia.route";
