7fa955e5e497c2dbaee708c4496d5481b8d3a440
luis44120 <luismora44120@gmail.com>
2020-10-26 09:13:38 -0300

    se crea nuevo componente (pagos)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PagosComponent } from './componets/pagos/pagos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagosComponent
   ],
   imports: [
     BrowserModule,
