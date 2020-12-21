23803015fd6049d9e0f7eb75496ef0a20ad80a18
SantiagoMardones <Ferricity@gmail.com>
2020-12-21 05:30:17 -0300

    Se crea la tabla categoria socio

--- frontend/src/app/components/categoria-socio/categoria-socio.component.ts
+++ frontend/src/app/components/categoria-socio/categoria-socio.component.ts
@@ -1,15 +1,97 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CategoriaService } from "../../services/categoria-socio.service";
+
+import {FormBuilder, FormGroup } from "@angular/forms";
+
+import { ICategoria_socio } from 'src/app/models/categoria_socio';
+
+
 @Component({
-  selector: 'app-categoria-socio',
-  templateUrl: './categoria-socio.component.html',
-  styleUrls: ['./categoria-socio.component.css']
+  selector:'app.categoria',
+  templateUrl: './categoria.component.html',
+  styleUrls: ['categoria.component.css']
 })
-export class CategoriaSocioComponent implements OnInit {
+export class CategoriaComponent implements OnInit {
 
-  constructor() { }
+  listcategoria =[];
 
-  ngOnInit(): void {
+  formCategoria: FormGroup;
+  p:number = 1;
+  buscarhorarios:any;
+
+  constructor(private categoriaServ:CategoriaService,private fb: FormBuilder) { 
+
+      this.formCategoria = this.fb.group({
+        id_categoria_socio:[null],
+        descripcion:['']
+
+      })
+
   }
+
+  ngOnInit(): void{
+    this.obtenercategoria();
+  }
+
+  obtenercategoria()
+{
+  this.categoriaServ.getcategoria().subscribe(
+    resultado => this.listcategoria = resultado,
+    error => console.log(error),
+  )
+}
+
+guardarcategoria()
+{
+
+  if(this.formCategoria.value.id_categoria_socio)
+  {
+    //si existe el id, se actualiza
+    this.categoriaServ.updatecategoria(this.formCategoria.value).subscribe(
+      respuesta => {
+          console.log(respuesta);
+          this.obtenercategoria();
+          this.formCategoria.reset();
+      },
+      error => console.log(error)
+    )
+  }else{
+    this.categoriaServ.savecategoria(this.formCategoria.value).subscribe(
+      resultado => {
+          console.log(resultado);
+
+          this.obtenercategoria();
+          this.formCategoria.reset();
+      },
+      error => console.log(error)
+    );
+  }
+
+ 
+}
+
+
+editarcategoria(categoria:ICategoria_socio)
+{
+  this.formCategoria.setValue(categoria);
+}
+
+eliminarcategoria(id:number)
+{
+
+  if(confirm('Estas seguro que deseas ejecutar esta accion?')){
+    this.categoriaServ.deletecategoria(id).subscribe(
+      respuesta => {
+        console.log(respuesta);
+        this.obtenercategoria();
+        alert(respuesta);
+      },
+      error => console.log(error)
+    );
+  }
+
+
+}
 
 }
--- frontend/src/app/services/categoria-socio.service.ts
+++ frontend/src/app/services/categoria-socio.service.ts
@@ -7,10 +7,32 @@ import { ICategoria_socio } from "../models/categoria_socio";
 @Injectable({
   providedIn: 'root'
 })
-export class CategoriaSocioService {
+export class CategoriaService {
 
-  constructor() {
+  constructor(private http:HttpClient) {
 
-   }
-   
+  }
+
+  getcategoria()
+  {
+    return this.http.get<ICategoria_socio[]>('http://localhost:3030/categoria/');
+  }
+
+  savecategoria(unCategoria:ICategoria_socio){
+    return this.http.post('http://localhost:3030/categoria/',unCategoria);
+    
+  }
+
+  updatecategoria(unCategoria:ICategoria_socio)
+  {
+    let id:number = unCategoria.id_categoria_socio;
+    return this.http.put('http://localhost:3030/categoria/'+id,unCategoria);
+  }
+
+  deletecategoria(id:number)
+  {
+    return this.http.delete('http://localhost:3030/categoria/'+id);
+  }
+
 }
+
